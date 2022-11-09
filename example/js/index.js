3404726a9eb77eec3a54ed536c009e9c4aff186e
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2022-11-09 08:23:29 +0100

    Får en nyckel vid onload

--- example/js/index.js
+++ example/js/index.js
@@ -1,22 +1,22 @@
-const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
-// API nyckel att använda ifall man enbart siktar på godkänt: solaris-vKkkQHqQboi7c6JF
+const BASE_URL = `https://fathomless-shelf-54969.herokuapp.com`;
+let API_KEY;
 
 
 async function getKey() {
-    const response = await fetch(`${BASE_URL}/keys`, { method: 'POST' });
-    const data = await response.json();
-    console.log(data);
+    const response = await fetch(`${BASE_URL}/keys`, {method: 'POST'})
+    API_KEY = await response.json();
+    console.log(API_KEY)
+    getPlanets();
 }
 
 async function getPlanets() {
     const response = await fetch(`${BASE_URL}/bodies`, {
         headers: {
-            'x-zocom': 'solaris-vKkkQHqQboi7c6JF'
+            'x-zocom': `${API_KEY.key}`
         }
     });
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json()
+    console.log(data)
 }
 
 getKey();
-getPlanets();
