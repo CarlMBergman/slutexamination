558c239b3e62c209cd5fb6a842a92b328cc2a490
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-02-02 13:58:17 +0100

    html som startsida, har dubbelt av all js

---
+++ js/index.js
@@ -0,0 +1,170 @@
+import { saveFavPlanet, getFavPlanets, manageFavPlanets } from "./modules/firebase.js";
+
+const BASE_URL          = 'https://my-json-server.typicode.com/zocom-christoffer-wallenberg/solaris-api'; // URL till api:n
+const planetsElem       = document.querySelectorAll(`article`); // Hämtar mina planeter
+const planetInformation = document.querySelector(`footer`); // Hämtar Informationssidan
+const solarSystem       = document.querySelector(`main`); // Hämtar solsystemet
+const planetInInfo      = document.querySelector(`#planetInInformation`); // Detta är planeten som syns på infosidan
+
+const favPlanetsBtn     = document.querySelector('#favPlanetsBtn')
+
+
+// async function getKey() { // Här hämtas nyckeln
+//     const response = await fetch(`${BASE_URL}/keys`);
+//     const API_KEY = await response.json();
+//     console.log(API_KEY)
+//     return API_KEY;
+// }
+
+async function getPlanets() {
+    // const API_KEY = await getKey()
+    // const response = await fetch(`${BASE_URL}/bodies`, {
+    //     headers: {
+    //         'x-zocom': `${API_KEY.key}` // Här låses API:n upp
+    //     }
+    // });
+    const response = await fetch('./js/data.json')
+    let planets = await response.json()
+    console.log(planets)
+    givePlanetInformation(planets)
+}
+
+async function givePlanetInformation(planets) {
+    for (let i = 0; i < planetsElem.length; i++) { // Loopar igenom mina planeter och lägger ett click på dom
+        planetsElem[i].addEventListener(`click`, () => {
+            planetInformation.classList.remove(`hide`)
+            solarSystem.classList.add(`hide`)
+            updateUIInformation(planets, i);
+            updatePlanetColors(planets, i)
+            console.log(planets[i])
+            favBtnClick(planets, i)
+        })
+    }
+}
+
+
+document.querySelector(`#rocketShip`).addEventListener(`click`, () => { // Klicka för att komma tillbaka till solsystemet, rensar även informationen om tidigare planet
+    planetInformation.classList.add(`hide`)
+    solarSystem.classList.remove(`hide`)
+    document.querySelector(`#information`).remove();
+    document.querySelector('#favBtn').remove();
+})
+
+function updateUIInformation(planets, i){  // Här skrivs all info ut
+    let el = `
+        <button id="favBtn">Save planet in favourites</button>
+        <aside id="information">
+            <div id="nameAndText">
+                <h1>${planets.bodies[i].name}</h1>
+                <h2>${planets.bodies[i].latinName}</h2>
+                <p>${planets.bodies[i].desc}</p>
+            </div>
+            <hr />
+            <div id="info">
+                <div
+                    id="
+                circumference"
+                >
+                    <h4>OMKRETS</h4>
+                    <p>${planets.bodies[i].circumference}</p>
+                </div>
+                <div id="fromSun">
+                    <h4>KM FRÅN SOLEN</h4>
+                    <p>${planets.bodies[i].distance}</p>
+                </div>
+                <div id="max">
+                    <h4>MAX TEMPERATUR</h4>
+                    <p>${planets.bodies[i].temp.day}</p>
+                </div>
+                <div id="min">
+                    <h4>MIN TEMPERATUR</h4>
+                    <p>${planets.bodies[i].temp.night}</p>
+                </div>
+            </div>
+            <hr />
+            <div id="moons">
+                <h4>MÅNAR</h4>
+            </div>
+        </aside>`
+
+    planetInformation.insertAdjacentHTML(`beforeend`, el)
+
+    let moons = planets.bodies[i].moons
+    for (let index = 0; index < moons.length; index++) { // Loopar igenom månarna och skriver ut dom en i varje grid fält
+        let moon = `<p class="moon">${planets.bodies[i].moons[index]}</p>`
+        document.querySelector(`#moons`).insertAdjacentHTML(`beforeend`, moon)
+    }
+}
+
+function updatePlanetColors(planets, i){ // Här ändras färgen på planeten i infosidan
+    if (planets.bodies[i].name === `Solen`) {
+        planetInInfo.style.backgroundColor = "rgba(255, 208, 41, 1)";
+        planetInInfo.style.boxShadow = "80px 0px 290px 0px #FFD029";
+    }
+    else if (planets.bodies[i].name === `Merkurius`) {
+        planetInInfo.style.backgroundColor = "rgba(136, 136, 136, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(136, 136, 136, 0.1), 0 0 0 100px rgba(136, 136, 136, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Venus`) {
+        planetInInfo.style.backgroundColor = "rgba(231, 205, 205, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(231, 205, 205, 0.1), 0 0 0 100px rgba(231, 205, 205, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Jorden`) {
+        planetInInfo.style.backgroundColor = "#428ED4";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(66, 142, 212, 0.1), 0 0 0 100px rgba(66, 142, 212, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Mars`) {
+        planetInInfo.style.backgroundColor = "rgba(239, 95, 95, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(239, 95, 95, 0.1), 0 0 0 100px rgba(239, 95, 95, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Jupiter`) {
+        planetInInfo.style.backgroundColor = "rgba(226, 148, 104, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(226, 148, 104, 0.1), 0 0 0 100px rgba(226, 148, 104, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Saturnus`) {
+        planetInInfo.style.backgroundColor = "rgba(199, 170, 114, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(199, 170, 114, 0.1), 0 0 0 100px rgba(199, 170, 114, 0.06)";
+        let saturnCircle = `<div id="saturnCircleBig"></div>`
+        document.querySelector(`#information`).insertAdjacentHTML(`beforeend`, saturnCircle)
+    }
+    else if (planets.bodies[i].name === `Uranus`) {
+        planetInInfo.style.backgroundColor = "rgba(201, 212, 241, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(201, 212, 241, 0.1), 0 0 0 100px rgba(201, 212, 241, 0.06)";
+    }
+    else if (planets.bodies[i].name === `Neptunus`) {
+        planetInInfo.style.backgroundColor = "rgba(122, 145, 167, 1)";
+        planetInInfo.style.boxShadow = "0 0 0 50px rgba(122, 145, 167, 0.1), 0 0 0 100px rgba(122, 145, 167, 0.06)";
+    }
+}
+
+function favBtnClick(planets, i) {
+    const favBtn = document.querySelector('#favBtn')
+    favBtn.addEventListener('click', () => {
+        manageFavPlanets(planets, i)
+        
+    })
+}
+
+favPlanetsBtn.addEventListener('click', () => {
+    getFavPlanets();
+    solarSystem.classList.add(`hide`)
+    document.querySelector('#allPlanetsBtn').classList.remove('hide')
+    back2Solarsystem();
+})
+
+function back2Solarsystem() {
+    const back2Planets = document.querySelector('#allPlanetsBtn')
+    
+    
+    back2Planets.addEventListener('click', () => {
+        const PlanetsHide = document.querySelectorAll('.favPlanetDiv')
+        solarSystem.classList.remove('hide')
+        back2Planets.classList.add('hide')
+        PlanetsHide.forEach((div) => {
+            div.remove()
+        });
+    })
+}
+
+
+getPlanets();
---
+++ js/modules/firebase.js
@@ -0,0 +1,101 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXEL9NtaXEAzXJo0M7lHibBF4QbBOSkEg",
+  authDomain: "solaris-f6c95.firebaseapp.com",
+  projectId: "solaris-f6c95",
+  storageBucket: "solaris-f6c95.appspot.com",
+  messagingSenderId: "164463429902",
+  appId: "1:164463429902:web:8a932b69a30fd42480af5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const favPlanetsList = document.querySelector('section')
+
+async function saveFavPlanet(planets, i) {
+    try {
+        await addDoc(collection(db, 'favPlanets'), {
+            planetName: planets.bodies[i].name,
+            planetSize: planets.bodies[i].circumference,
+            DistanceFromEarth: planets.bodies[i].distance
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function removeFromDatabase(planetId) {
+    try {
+        await deleteDoc(doc(db, 'favPlanets', planetId))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function addRemoveClick() {
+    const noFavBtns = document.querySelectorAll('.noFavBtn')
+
+    noFavBtns.forEach((btn) => {
+        btn.addEventListener('click', async (event) => {
+            const planetId = event.target.getAttribute('data-planet-id');
+            await removeFromDatabase(planetId)
+
+            const planetsHide = document.querySelectorAll('.favPlanetDiv')
+            planetsHide.forEach((div) => {
+                div.remove()
+             });
+            getFavPlanets()
+        })
+    })
+}
+
+async function getFavPlanets() {
+    const favPlanets = await getDocs(collection(db, 'favPlanets'));
+
+    favPlanets.forEach((favPlanet) => {
+        const elem = `
+        <div class="favPlanetDiv">
+            <h1>${favPlanet.data().planetName}</h1>
+            <div class="${favPlanet.data().planetName}"></div>
+            <button class="noFavBtn" data-planet-id="${favPlanet.id}">Remove planet from favourites</button>
+        </div>    
+            `
+            favPlanetsList.insertAdjacentHTML('beforeend', elem)
+    });
+    addRemoveClick()
+}
+
+async function checkIfAlreadyFav(planets, i) {
+    try {
+        const planetNameQuery = query(collection(db, 'favPlanets'), where('planetName', '==', planets.bodies[i].name));
+        const result = await getDocs(planetNameQuery);
+        let resultName = {};
+
+        result.forEach((planetName) => {
+            console.log(planetName);
+            resultName = planetName;
+        });
+        return resultName;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function manageFavPlanets(planets, i) {
+    const planetName = await checkIfAlreadyFav(planets, i)
+    const planetId = planetName.id
+    if (planetId) {
+        alert('Already saved in favourites!')
+    } else {
+        saveFavPlanet(planets, i)
+    }
+}
+
+export { saveFavPlanet, getFavPlanets, manageFavPlanets }
